99c810b9aa1698f5fcc6211c8ec787fec68966a4
Jakub Janak <janakja5@fel.cvut.cz>
2025-12-10 11:23:58 +0100

    frontend created

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // all files using className
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  // this is important for NativeWind v4
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
