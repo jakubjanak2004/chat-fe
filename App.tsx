9041139c2233f19e4d12ae499a443bfe20701c12
Jakub Janak <janakja5@fel.cvut.cz>
2025-12-10 11:41:00 +0100

    the nativewind created and working

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
 import "./global.css";
-import { Text, View } from 'react-native';
+import {Pressable, Text, View} from 'react-native';
+
 
 export default function App() {
-  return (
-      <View className="flex-1 bg-black items-center justify-center">
-        <Text className="text-white text-3xl font-bold">
-          Hello hello hello does this render? hi
-        </Text>
-      </View>
-  );
+    console.log("App rendered ✅");
+
+    return (
+        <View className="flex-1 items-center justify-center">
+            <Pressable className="bg-white rounded-xl p-4 mb-3">
+                <Text className="text-black font-semibold text-lg">
+                    Article title
+                </Text>
+                <Text className="text-neutral-500 text-sm mt-1">
+                    21.10., 10:46
+                </Text>
+            </Pressable>
+        </View>
+    );
 }
