600b5f7f80dd164712c49940c48bf84026c79795
Jakub Janak <janakja5@fel.cvut.cz>
2026-03-27 15:51:58 +0100

    added handling the URLs through environment variables

--- app/config/Env.ts
+++ app/config/Env.ts
@@ -2,13 +2,11 @@ export const CONFIG = {
     // paging
     PAGE_SIZE: 20,
     // rest
-    // API_URL: "http://127.0.0.1:8090",
-    API_URL: "https://8713-147-32-108-101.ngrok-free.app",
+    API_URL: process.env.EXPO_PUBLIC_API_URL,
     TIMEOUT_MS: 10_000,
     SEPARATOR_GAP_MIN: 15,
     // ws
-    // WS_URL: "http://127.0.0.1:8090/ws",
-    WS_URL: "https://8713-147-32-108-101.ngrok-free.app/ws",
+    WS_URL: process.env.EXPO_PUBLIC_WS_URL,
     WS_RECONNECT_DELAY:  10_000,
     WS_HEARTBEAT_INCOMING: 10_000,
     WS_HEARTBEAT_OUTGOING: 10_000,
