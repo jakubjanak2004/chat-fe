72eac513f6d985659034d820539360f214fc7316
Jakub Janak <janakja5@fel.cvut.cz>
2026-03-27 14:43:15 +0100

    refresh logic working on web as we use local storage

--- app/context/TokenStorage.ts
+++ app/context/TokenStorage.ts
@@ -1,14 +1,26 @@
 import * as SecureStore from 'expo-secure-store';
+import {Platform} from "react-native";
 import {CONFIG} from "../config/Env";
 
 export async function saveRefreshToken(refreshToken: string): Promise<void> {
+    if (Platform.OS === "web") {
+        window.localStorage.setItem(CONFIG.REFRESH_TOKEN_KEY, refreshToken);
+        return;
+    }
     await SecureStore.setItemAsync(CONFIG.REFRESH_TOKEN_KEY, refreshToken);
 }
 
 export async function getRefreshToken(): Promise<string | null> {
+    if (Platform.OS === "web") {
+        return window.localStorage.getItem(CONFIG.REFRESH_TOKEN_KEY);
+    }
     return await SecureStore.getItemAsync(CONFIG.REFRESH_TOKEN_KEY);
 }
 
 export async function clearRefreshToken(): Promise<void> {
+    if (Platform.OS === "web") {
+        window.localStorage.removeItem(CONFIG.REFRESH_TOKEN_KEY);
+        return;
+    }
     await SecureStore.deleteItemAsync(CONFIG.REFRESH_TOKEN_KEY);
 }
